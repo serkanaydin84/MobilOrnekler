eb6f5dbb79954fd2072649607220ae494a20e867
BIL-LAB5OGRT\Ogrenci <123@123.COM>
2022-12-23 16:03:21 +0300

    no message

--- app/src/main/java/com/example/mobiluygulamalardersornekleri/U4_GoldSoru3.java
+++ app/src/main/java/com/example/mobiluygulamalardersornekleri/U4_GoldSoru3.java
@@ -3,52 +3,55 @@ package com.example.mobiluygulamalardersornekleri;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class U4_GoldSoru3 extends AppCompatActivity {
 
-    EditText txtTorba1, txtTorba2, txtGrupA, txtGrupB, txtGrupC, txtGrupD;
+    ListView listTorba1, listTorba2;
+    EditText txtGrupA, txtGrupB, txtGrupC, txtGrupD;
+
+    private String[] arrayTorba1 = {"Almanya", "Fransa", "İngiltere", "Brezilya", "Arjantin", "Belçika", "İspanya", "Hırvatistan"};
+    private String[] arrayTorba2 = {"TÜRKİYE", "Fas", "Senegal", "Qatar", "Meksika", "Japonya", "Suudi Arabistan"};
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.u2_goldsoru3);
 
-        txtTorba1 = findViewById(R.id.txtTorba1);
-        txtTorba2 = findViewById(R.id.txtTorba2);
+        listTorba1 = findViewById(R.id.listTorba2);
+        listTorba2 = findViewById(R.id.listTorba2);
         txtGrupA = findViewById(R.id.txtGrupA);
         txtGrupB = findViewById(R.id.txtGrupB);
         txtGrupC = findViewById(R.id.txtGrupC);
         txtGrupD = findViewById(R.id.txtGrupD);
 
-        txtTorba1.setText("Almanya");
-        txtTorba1.setText(txtTorba1.getText() + "\n" + "Fransa");
-        txtTorba1.setText(txtTorba1.getText() + "\n" + "İngiltere");
-        txtTorba1.setText(txtTorba1.getText() + "\n" + "Brezilya");
-        txtTorba1.setText(txtTorba1.getText() + "\n" + "Arjantin");
-        txtTorba1.setText(txtTorba1.getText() + "\n" + "Belçika");
-        txtTorba1.setText(txtTorba1.getText() + "\n" + "İspanya");
-        txtTorba1.setText(txtTorba1.getText() + "\n" + "Hırvatistan");
+        ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, android.R.id.text1, arrayTorba1);
+        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_2, android.R.id.text2, arrayTorba2);
 
-        txtTorba2.setText("TÜRKİYE");
-        txtTorba2.setText(txtTorba2.getText() + "\n" + "Hollanda");
-        txtTorba2.setText(txtTorba2.getText() + "\n" + "Fas");
-        txtTorba2.setText(txtTorba2.getText() + "\n" + "Senegal");
-        txtTorba2.setText(txtTorba2.getText() + "\n" + "Qatar");
-        txtTorba2.setText(txtTorba2.getText() + "\n" + "Meksika");
-        txtTorba2.setText(txtTorba2.getText() + "\n" + "Japonya");
-        txtTorba2.setText(txtTorba2.getText() + "\n" + "Suudi Arabistan");
+        listTorba1.setAdapter(arrayAdapter1);
+        listTorba2.setAdapter(arrayAdapter2);
     }
 
     public void btnKura (View view) {
         Random random = new Random();
-        int kura = random.nextInt(16);
-        for (int i = 1; i <= 16; i++) {
+
+        int torba1 = arrayTorba1.length;
+        int torba2 = arrayTorba2.length;
+        System.out.println(torba1 + torba2);
+        int donguSonu = torba1 + torba2;
+
+        for (int i = 1; i <= donguSonu; i++) {
+            int kura = random.nextInt(donguSonu);
             if (i % 4 == 0) {
 
             } else if (i % 4 == 1) {
